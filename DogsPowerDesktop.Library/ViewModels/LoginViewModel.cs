ac59ebf39a384c8e6793e2fef5da9547d1b96058
Jane Moroz <Moroz.jane@yahoo.com>
2021-06-18 18:35:53 +0300

    Create Login Page

---
+++ DogsPowerDesktop.Library/ViewModels/LoginViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DogsPowerDesktop.Library
+{
+    /// <summary>
+    /// The View Model for a login screen
+    /// </summary>
+    public class LoginViewModel : BaseViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The login of the user
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// A flag indicating if the login command is running
+        /// </summary>
+        public bool LoginIsRunning { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// The command to login
+        /// </summary>
+        public ICommand LoginCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public LoginViewModel()
+        {
+            // Create commands
+            LoginCommand = new RelayParameterizedCommand(async (parameter) => await LoginAsync(parameter));
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Attempts to log the user in
+        /// </summary>
+        /// <param name="parameter">The <see cref="SecureString"/> passed in from the view for the users password</param>
+        /// <returns></returns>
+        public async Task LoginAsync(object parameter)
+        {
+            await RunCommandAsync(() => LoginIsRunning, async () =>
+            {
+                // TODO: Fake a login...
+                await Task.Delay(1000);
+            });
+        }
+    }
+}
